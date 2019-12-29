1df3753a0726ab128a049074dd4f96a030c476f9
jay23cee <jayceexxiii@gmail.com>
2019-12-28 21:35:49 -0800

    Added delete and update link

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 LEFT OFF AT 29:00
 
-having an error with app.py line 27 : Task query not found. could be because of SQLAlchemy model?
-getting a nontype error for ORDER_BY 
+https://www.youtube.com/watch?v=Z1RJmh_OqeA&pbjreload=10
+
+having an error with app.py line 27 : Task query not found. could be because of SQLAlchemy model?   // Needed to update flask
+getting a nontype error for ORDER_BY // Fixed. needed to delete create_all() on the TODO function.
+
+
+12/23/19 : As of Today the road is clear to continue this project. Was able to connect the CSS style . It seems cache uploads
+it's version before updating the css. To view the updated stylesheet we need to use CTRL AND F5. 
+
+1:03am 12/24/19
+Update and delete are working properly.
+
--- app.py
+++ app.py
@@ -11,11 +11,8 @@ db=  SQLAlchemy(app)
 
 
 class Todo(db.Model) :
-    __abstract__ = True
-    db.create_all()
     id = db.Column(db.Integer, primary_key = True)
     content = db.Column(db.String(200), nullable= False)
-    completed =  db.Column(db.Integer, default =0)
     date_created = db.Column(db.DateTime, default = datetime.utcnow)
 
     def __repr__(self):
@@ -23,9 +20,10 @@ class Todo(db.Model) :
 
 @app.route('/' , methods = ['POST', 'GET'])
 def index():
+   
     if request.method =='POST':
         task_content = request.form['content']
-        new_task = Todo(content= task_content)
+        new_task = Todo(content = task_content)
 
         try:
             db.session.add(new_task)
@@ -36,7 +34,39 @@ def index():
 
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
+        
+      #  return render_template('index.html', tasks=tasks)
         return render_template('index.html', tasks=tasks)
-    #return "Hellow world"
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    print(id)
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+
+@app.route('/update/<int:id>', methods =['GET', 'POST'])
+def update(id):
+    
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+
+    else:
+        return render_template('update.html', task=task)
+
 if __name__ ==  "__main__":
     app.run(debug=True)
